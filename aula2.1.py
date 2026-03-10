c35341297796b16270b0b70caa548a5cdf96fc86
pablog-hub <pablogomesdosanjos@gmail.com>
2026-03-09 21:00:08 -0300

    9/3/2026

---
+++ aula2.1.py
@@ -0,0 +1,42 @@
+#Exercício 1
+'''nome =  'Pablo Gomes dos Anjos'
+print(nome)
+
+#Exercício 2
+a = 3
+b = 5
+mult1 = 2*a
+mult2 = 3*b
+resul1 = mult1 * mult2
+print(resul1)
+
+#Exercício 3
+c = 3
+d = 5
+e = 10.55
+soma = c + d + e
+print(f'Soma = {soma:.3f}')
+
+#Exercício 4
+number1 = int(input('Número 1: '))
+number2 = int(input('Número 2: '))
+resul2 = number1 + number2
+print(f'resultado: {resul2}')
+
+#Exercício 5
+Metros = float(input('Metros: '))
+print(f'Milimitros {Metros*1000}')
+
+#Exercício 6
+dias = int(input('Dias:'))
+horas = int(input('Horas:'))
+minutos = int(input('Minutos:'))
+segundos = int(input('Segundos:'))
+print(f' Seu valor em segundos: {dias*86400 + horas*3600 + minutos*60 + segundos}')
+
+#Exercício 7'''
+salario = float(input('Seu salário:'))
+aumentoPorcentagem = float(input('Porcentagem do aumento: '))
+porcentSalario = salario * aumentoPorcentagem/100
+novoSalario = salario + porcentSalario
+print(f'Seu novo salário é: {novoSalario:.0f}')
---
+++ aula2.py
@@ -0,0 +1,19 @@
+# meu primeiro programa
+''' my first program'''
+print('Heloo Word!')
+
+check1 = 8
+check2 = 5.5
+soma = check1 + check2
+print(soma)
+
+media = (check1 + check2)/2
+print(media)
+
+print(type(check1))
+print(type(check2))
+print(type(soma))
+print(type(media))
+
+nome = 'Pablo Gomes dos Anjos'
+print(type(nome))
