88e34fd302ba012b39fee066e2ff2312ff515f8d
pablog-hub <pablogomesdosanjos@gmail.com>
2026-03-15 19:40:21 -0300

    exercícios aula 2

--- aula2.1.py
+++ aula2.1.py
@@ -34,9 +34,51 @@ minutos = int(input('Minutos:'))
 segundos = int(input('Segundos:'))
 print(f' Seu valor em segundos: {dias*86400 + horas*3600 + minutos*60 + segundos}')
 
-#Exercício 7'''
+#Exercício 7
 salario = float(input('Seu salário:'))
 aumentoPorcentagem = float(input('Porcentagem do aumento: '))
 porcentSalario = salario * aumentoPorcentagem/100
 novoSalario = salario + porcentSalario
 print(f'Seu novo salário é: {novoSalario:.0f}')
+
+#Exercício 8
+preco = float(input('Preço da mercadoria:'))
+aumentoPorcent = float(input('Porcentagem do desconto:'))
+calculo = preco * aumentoPorcent/100
+novoPreco = preco - calculo
+print(f'O preço com desconto é: {novoPreco:.2f}')
+
+#Exercício 9
+distancia = float(input('Qual a distância do trajeto (em km): '))
+velocidade = float(input('Qual é a velociadade média do trajeto (km/h): '))
+tempo = distancia /  velocidade
+horas = int(tempo)
+minutos = int((tempo - horas) * 60)
+if tempo == 1:
+    print('A viagem levará exatamente 1 hora.')
+else:
+    print(f'A viagem levará {horas:.0f} horas e {minutos}  minutos.')
+
+#Exercício 10
+temperaturaC = float(input('Digite a temperatura em C:'))
+temperaturaF = 9 * temperaturaC/5 + 32
+print(f'A temperatura em Fahrenheit é {temperaturaF}')
+
+#Exercício 11
+quantidadeKm = float(input('Quantos Km foram rodados com o carro:'))
+quantidadeDias = float(input('Quantos dias você usou o carro:'))
+preco1 = quantidadeKm * 0.15
+preco2 = quantidadeDias * 60
+print(f'O preço a pagar é: R${preco1 + preco2}.')
+
+#Exercicio12
+x = int(input('Digite o valor de x: '))
+y = int(input('Digite o valor de y: '))
+z = (x**2 + y**2)/ (x - y)**2
+print(f'O valor de z é: {z}')
+
+#Exercício 13
+salario1 = float(input('Digite seu salário: '))
+aumentoS1 = salario1 * 35/100
+novoS1  = salario1 + aumentoS1
+print(f'Seu novo salário é R${novoS1:.2f}')'''
