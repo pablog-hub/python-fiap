07a344d8e7da87971176a8bb20b05c292fd59b07
pablo-hub <pablogomesdosanjos@gmail.com>
2026-03-30 20:57:56 -0300

    Exercícios

---
+++ aula4.py
@@ -0,0 +1,56 @@
+# Exercício 1
+# x = 1
+# while x<=100:
+#     print(x)
+#     x+=1
+
+#Exercício 2
+# x = 50
+# while x <= 100:
+#     print(x)
+#     x+=1
+
+#Exercício 3
+# import time
+# x = 10
+# while x>= 1:
+#     print(x)
+#     time.sleep(0.7)
+#     x-=1
+# print('FOGOOOO!!!')
+
+#Exercício 4
+# fim = int(input("Digite o último número a imprimir: "))
+# x = 1
+# while x <= fim:
+#  print(x)
+#  x += 2
+
+#Exercício 5
+# x = 3
+# y = 1
+# while y <= 10:
+#     resultado = x * y
+#     print(resultado)
+#     y+=1
+
+#Exercício 6
+# x = int(input('Tabuada do: '))
+# y = 0
+# while y <= 10:
+#     resultado = x * y
+#     print(f'{x} x {y} = {resultado}')
+# y+=1
+
+#Exercício 7
+# x = int(input('Tabuada do: '))
+# z = int(input('até: '))
+# y = 1
+# while y <= z:
+#     resultado = x * y
+#     print(f'{x} x {y} = {resultado}')
+#     y+=1
+
+#Exercicio 8
+dp = float(input('Digite seu depósito inicial: '))
+taxa = float(input('Digite a taxa (em %)'))
